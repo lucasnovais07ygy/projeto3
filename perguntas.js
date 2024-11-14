ccf5e60461696a55dd861fcb876596310ac31a5c
lucasnovais07ygy <00001089165717sp@al.educacao.sp.gov.br>
2024-11-14 12:13:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Futebol',
+    'Qual é o jogador com mais gols na carreira?',
+    'Cristiano Ronaldo com 908 gols'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual o artilheiro com mais gols de falta na atualidade?',
+    'Lionel Messi com 57 gols'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual  melhor time do mundo?',
+    'Melhor time do mundo é o corinthans'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual é o jogador com mais bolas de ouro?',
+    'Lionel Messi com 8 bolas de ouro'
+)
